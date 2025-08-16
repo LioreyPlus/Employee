e2e2fae080be3936252b5be3f3041db49f33d3f2
LioreyPlus <DeaDFlame2015@gmail.com>
2025-08-16 17:32:05 +0300

    Добавлено новое поле Role Удалены классы Manager и Developer Добавлено нопое поле ID

---
+++ src/main/java/org/example/Role.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public enum Role {
+    EMPLOYEE {
+        @Override
+        public void work(String name, String title, int salary) {
+            System.out.printf("Hi! I'm a worker, my name is %s, and I live on a salary of %d rubles%n", name, salary);
+        }
+    },
+    MANAGER {
+        @Override
+        public void work(String name, String title, int salary) {
+            System.out.printf("Hi! I am in the position of %s. My name is %s and I get %d rubles%n", title.toLowerCase(), name, salary);
+        }
+    },
+    DEVELOPER {
+        @Override
+        public void work(String name, String title, int salary) {
+            System.out.printf("Hi! My name is %s. I am in the position of %s and I earn %d rubles%n", name, title.toLowerCase(), salary);
+        }
+    };
+
+    public abstract void work(String name, String title, int salary);
+}
