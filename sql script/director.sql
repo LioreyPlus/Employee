3512d2f004000f0299f51a570e778c06e540894c
LioreyPlus <DeaDFlame2015@gmail.com>
2025-09-04 14:24:55 +0300

    sql скрипты

---
+++ sql script/director.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (id, name, salary, scheduleType, title, role)
+VALUES (1, 'Alex Howell, 439622, 5/2, 'CEO', 'DIRECTOR');
---
+++ sql script/employee_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  salary INT,
+  scheduleType VARCHAR(5),
+  title VARCHAR(15),
+  role VARCHAR(15),
+  chief INT(10)
+);
